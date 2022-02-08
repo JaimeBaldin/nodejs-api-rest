b3bac8c0f76186c631dffa61ff4202e54da4fb6c
jaimebaldin <you@example.com>
2022-02-08 05:58:17 -0300

    Tratando datas no banco e passando status HTTP

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -6,7 +6,7 @@ module.exports = app => {
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body
 
-        Atendimento.adiciona(atendimento)
-        res.send('Você está na rota de atendimento POST')
+        Atendimento.adiciona(atendimento, res)
+        
     })    
 }
--- models/atendimento.js
+++ models/atendimento.js
@@ -1,16 +1,49 @@
+const res = require('express/lib/response')
+const moment = require('moment')
+
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento {
-    adiciona(atendimento) {
-        const sql = 'INSERT INTO Atendimento SET ?'
+    adiciona(atendimento,res) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
+        
+        const dataValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteValido = atendimento.cliente.length >= 3
 
-        conexao.query(sql, atendimento,(erro, resultados) => {
-            if(erro) {
-                console.log(erro)
-            } else {
-                console.log(resultados)
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataValida,
+                mensagem: 'Data deve ser maior ou igual a atual'    
+            },
+            {
+                nome: 'cliente',
+                valido: clienteValido,
+                mensagem: 'Cliente deve ter pelo menos 3 caracteres'
             }
-        })
+        ]
+
+        const erros =  validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if(existemErros) {
+            res.status(400).json(erros)
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+            const sql = 'INSERT INTO Atendimento SET ?'
+
+
+            conexao.query(sql, atendimentoDatado,(erro, resultados) => {
+                if(erro) {
+                    res.status(400).json(erro)
+                } else {
+                    res.status(201).json(resultados)
+                }
+            })
+        }
+
+        
     }
 }
 

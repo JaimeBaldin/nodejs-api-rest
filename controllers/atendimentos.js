8c105b8d3ed19bb7bf71052697279cf9edea6b3c
jaimebaldin <you@example.com>
2022-02-07 22:23:14 -0300

    Criando a primeira tabela e alimentando ela.

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,8 +1,12 @@
+const Atendimento = require('../models/atendimento')
+
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Voce esta na rota atendimento GET'))
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body)  
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento)
         res.send('Você está na rota de atendimento POST')
     })    
 }
